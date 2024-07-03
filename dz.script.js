69d8dfc6f1158654e08eb00de9798a5f8bcdafc7
Razvan Carp <razvan_ector@yahoo.com>
2024-07-03 20:53:21 +0300

    website 1.2

--- dz.script.js
+++ dz.script.js
@@ -1,5 +1,18 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    // de adăugat aici JS pentru un site interactiv
-    console.log('Site-ul este gata!');
-});
+// notatie camel case
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const overlay = document.querySelector('.overlay');
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+
+const openMobileMenu = () => {
+    overlay.style.display = 'block';
+    mobileMenuContainer.style.transform = 'translateX(0px)';
+};
+
+mobileMenuIcon.addEventListener('click', openMobileMenu);
+
+const closeMobileMenu = () => {
+    overlay.style.display = 'none';
+    mobileMenuContainer.style.transform = 'translateX(360px)';
+};
+
+overlay.addEventListener('click', closeMobileMenu);
